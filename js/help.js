20582819dc7c28a22fba541c8bb071689be55d9f
Max Waterman <max.waterman@intel.com>
2016-04-07 15:48:53 +0100

    Using debug version.

--- js/help.js
+++ js/help.js
@@ -1,2 +1,32 @@
-"use strict";function helpInit(a,b){var c=document.getElementById(a),d=document.getElementById(b+"dialog"),e=document.getElementById(b+"close");c.addEventListener("click",function(){d.className="helpdialog shown"}),e.addEventListener("click",function(){d.className="helpdialog"})}
+'use strict';
+
+/*
+ * Copyright (c) 2012, Intel Corporation.
+ *
+ * This program is licensed under the terms and conditions of the
+ * Apache License, version 2.0.  The full text of the Apache License is at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+
+/* exported helpInit */
+
+/**
+ * Add click handlers for the help dialog.
+ * @param {string} btnid The id of the help button.
+ * @param {string} prefix The prefix of the help elements.
+ */
+function helpInit(btnid, prefix) {
+  var btn = document.getElementById(btnid);
+  var dialog = document.getElementById(prefix + 'dialog');
+  var close = document.getElementById(prefix + 'close');
+
+  btn.addEventListener('click', function () {
+    dialog.className = 'helpdialog shown';
+  });
+
+  close.addEventListener('click', function () {
+    dialog.className = 'helpdialog';
+  });
+}
 //# sourceMappingURL=help.js.map
